0f340c1f132677abafe49948895a11092dfb9277
denis <pvl-den@yandex.ru>
2021-06-01 21:48:55 +0700

    changing pom

---
+++ homework-02/src/main/java/ru/otus/dao/QuestionsDao.java
@@ -0,0 +1,10 @@
+package ru.otus.dao;
+
+import ru.otus.domain.Question;
+
+import java.util.List;
+
+public interface QuestionsDao {
+
+    List<Question> getAllQuestions();
+}
---
+++ homework-02/src/main/java/ru/otus/service/QuestionsServiceImpl.java
@@ -0,0 +1,20 @@
+package ru.otus.service;
+
+import ru.otus.dao.QuestionsDao;
+import ru.otus.domain.Question;
+
+import java.util.List;
+
+public class QuestionsServiceImpl implements QuestionsService {
+
+    private final QuestionsDao questionsDao;
+
+    public QuestionsServiceImpl(final QuestionsDao questionsDao) {
+        this.questionsDao = questionsDao;
+    }
+
+    @Override
+    public List<Question> getAllQuestions() {
+        return questionsDao.getAllQuestions();
+    }
+}
