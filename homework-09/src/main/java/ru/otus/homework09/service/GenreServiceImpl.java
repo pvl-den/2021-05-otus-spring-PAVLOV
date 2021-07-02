3ca931e709265d82e782cf382ebcc1f50af1e6cc
pvl-den <pvl-den@yandex.ru>
2021-07-02 19:53:38 +0300

    hw9

--- homework-09/src/main/java/ru/otus/homework09/service/GenreServiceImpl.java
+++ homework-09/src/main/java/ru/otus/homework09/service/GenreServiceImpl.java
@@ -2,6 +2,7 @@ package ru.otus.homework09.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.otus.homework09.entity.Genre;
 import ru.otus.homework09.repository.GenreRepository;
 
@@ -12,6 +13,7 @@ public class GenreServiceImpl implements GenreService {
     final GenreRepository genreRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public Genre getById(final long id) {
         return genreRepository.getById(id);
     }
